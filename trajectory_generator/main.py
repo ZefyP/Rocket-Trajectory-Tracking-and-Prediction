cb2e7c0f3e37d6fd32d99a6edf5c84b3c1df2a3a
ZefyP <zefypi@gmail.com>
2023-05-08 21:01:57 +0100

    rename cli.py to main.py and add usage message

---
+++ trajectory_generator/main.py
@@ -0,0 +1,39 @@
+"""Command Line Interface for the trajectory generator"""
+import argparse
+
+parser = argparse.ArgumentParser(description='Generate a trajectory.')
+
+parser.add_argument('--verbose,-v', type=str, action = 'store_true', help='print the flight log during simulation')
+parser.add_argument('--output', type=str, help='output file name')
+parser.add_argument('--help', type=str, help='show instructions')
+
+args = parser.parse_args()
+
+if not any(vars(args).values()):
+    parser.print_help()
+else:
+    # generate the trajectory based on the given arguments
+    pass # replace this with your actual code to generate the trajectory
+
+# Add a usage message
+if __name__ == '__main__':
+    args = parser.parse_args()
+        
+    # Add a usage message
+    if __name__ == '__main__':
+        print('Usage: trajectory_generator [OPTIONS]\n')
+        print('Options:')
+        # print('  --step INT       number of time step to compute (in seconds)')
+        print('  --output str      output file name')
+        print('  -v, --verbose     print the flight log during simulation')
+
+        print('[!] In this simulation:')
+        
+    if args.verbose:
+        print("Verbose mode is on.")
+    
+    if args.output:
+        print(f"Output file name: {args.output}")
+    
+    if args.help:
+        parser.print_help()
